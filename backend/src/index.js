dd0ea852a55de4f4f4f3d871e4d332b3168db4e4
Ambadi <abcsavnk2@gmail.com>
2026-07-15 21:14:34 +0530

    Add Dockerfile and .dockerignore; implement static file serving in Express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors'; // Browser security rules, to enable which all urls can access this server
 import dotenv from 'dotenv'; dotenv.config();
+import fs from 'fs';
+import path from 'path';
+
+
 import User from './models/user.model.js';
 import connectDB from './lib/db.js';
 import { clerkMiddleware } from '@clerk/express';
@@ -11,6 +15,8 @@ const app = express();
 const PORT = process.env.PORT
 const frontendUrl = process.env.FRONTEND_URL
 
+const publicDir = path.join(process.cwd(), 'public');
+
 app.use(express.json());
 
 //app.use(cors()); // all sites can access
@@ -20,9 +26,25 @@ app.use(cors({
 }));
 app.use(clerkMiddleware());
 
+
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello World" });
 });
+
+// Serve static files from public directory
+// this is for production build
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+
+    // Handle all other routes by serving the index.html file
+    app.get('{*}', (req, res, next) => {
+        res.sendFile(path.join(publicDir, 'index.html'), (err) => {
+            if (err) {
+                next(err);
+            }
+        });
+    });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
