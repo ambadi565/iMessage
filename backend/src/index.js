09c62d2ae6720d22551c6e2c858caba4cc27ad12
Ambadi <abcsavnk2@gmail.com>
2026-07-15 20:31:37 +0530

    Add user and message models, database connection, and update server configuration
    - Implemented user and message schemas in Mongoose.
    - Created a database connection utility.
    - Updated server to include CORS configuration and Clerk middleware.
    - Added a basic route for testing server response.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,30 @@
 import express from 'express';
+import cors from 'cors'; // Browser security rules, to enable which all urls can access this server
 import dotenv from 'dotenv'; dotenv.config();
+import User from './models/user.model.js';
+import connectDB from './lib/db.js';
+import { clerkMiddleware } from '@clerk/express';
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT
+const frontendUrl = process.env.FRONTEND_URL
+
+app.use(express.json());
+
+//app.use(cors()); // all sites can access
+app.use(cors({
+    origin: frontendUrl,
+    credentials: true
+}));
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
+  connectDB();  
 });
